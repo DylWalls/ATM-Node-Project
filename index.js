89a8e8f416f6a50b2f920b565362cba563f45b36
Dylan Walls <dylanwalls410@gmail.com>
2021-07-02 21:22:52 -0600

    Changed the Userprompt to a console with a userinput underneath.

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function menuRoute(str){
 
 function menu(){ //ATM Menu
         pinVerify();
-         console.log("What would you like to do? \n Press 1 for Balance  \n 2 for Withdraw  \n 3 for Deposit check or cash. ") //Prompting user to make a choice
+          
+        console.log("What would you like to do? \n Press 1 for Balance  \n 2 for Withdraw  \n 3 for Deposit check or cash. ") //Prompting user to make a choice
         let userInput = prompt();
         menuRoute(userInput);
 }
